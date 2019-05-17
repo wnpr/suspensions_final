3b436fc6c455b6fb393e63e8f843d0859199a854
Patrick <pskahill@ctpublic.org>
2019-05-17 14:53:02 -0400

    third commit

--- suspensions_final.R
+++ suspensions_final.R
@@ -5,14 +5,24 @@ library(ggplot2)
 disability <- read.csv("disability-17-18.csv", header = T)
 
 
-chart_1 <- disability %>% 
+p <- disability %>% 
   ggplot()+
-  geom_col(stat= "identity",mapping = aes(x=reorder(Primary.Disability, -Suspension.Rate), y=Suspension.Rate), fill="purple")+
+  geom_col(stat= "identity",mapping = aes(x=reorder(Primary.Disability, -Suspension.Rate), y=Suspension.Rate), fill="#0072B2")+
+  theme_minimal()+
+  theme(panel.border = element_blank(), panel.grid.major = element_blank())+
   labs(y = "Percentage")+
   labs(x = "Disability Type")+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   theme(axis.text.y = element_text(angle = 45, hjust = 1))+
   labs(title = "Estimated Statewide Suspension Rate for Students with Disabilities By Primary Disability",
-       subtitle = "Source: CT Department of Education")
+       subtitle = "Source: CT Department of Education")+
+  theme(plot.title = element_text(face = "bold"))
 
-chart_1
+p
+
+
+
+
+
+
+
